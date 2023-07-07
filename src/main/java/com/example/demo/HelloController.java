3eba103ecdab87969c46f6fa39b02bc27c273f3f
devCJH <http-@naver.com>
2023-07-07 15:12:03 +0900

    카카오 로그인 안됨

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,15 +4,29 @@ import com.example.demo.question.DataNotFoundException;
 import com.example.demo.question.Question;
 import com.example.demo.question.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
 public class HelloController {
     @Autowired
     QuestionRepository questionRepository;
+
     @GetMapping("/extest")
     public String extest() {
         Optional<Question> oq = questionRepository.findById(123123);
@@ -33,6 +47,46 @@ public class HelloController {
         return "/fragment/nav.html";
     }
 
-//    /sakljdflaksjfd;lkdfasdfaklsdfjklasdjfaasdfkjlksajdfklj
-    //sdfsdfsdf
+    @GetMapping("/auth")
+    public String auth(Model model) {
+        String targetUrl = "https://kauth.kakao.com/oauth/authorize";
+        String clientId = "a0072d1885fe5cbccfeb539059439e9f";
+        String redirectUri = "http://localhost:8088/login/auth2/code/kakao";
+        String responseType = "code";
+
+        try {
+            redirectUri = URLEncoder.encode(redirectUri, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, String> param = new HashMap<>();
+        param.put("targetUrl", targetUrl);
+        param.put("clientId", clientId);
+        param.put("redirectUri", redirectUri);
+        param.put("repsonseType", responseType);
+
+        model.addAttribute("client_id", clientId);
+        model.addAttribute("redirect_uri", redirectUri);
+        model.addAttribute("response_type", responseType);
+
+        return "auth_test";
+
+    }
+
+
+////    /sakljdflaksjfd;lkdfasdfaklsdfjklasdjfaasdfkjlksajdfklj
+//    //sdfsdfsdf
+
+
+    @GetMapping("regform")
+    public String regform() {
+        return "/note_test";
+    }
+    @GetMapping("regtest")
+    public String regtest(@RequestParam("editordata") String data) {
+        System.out.println("123123");
+        System.out.println(data);
+        return "redirect:/question/list";
+    }
 }
--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,15 +8,42 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    CustomerOAuth2UserService customOAuth2UserService;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().requestMatchers(
@@ -23,6 +51,9 @@ public class SecurityConfig {
         )
                 .permitAll()
                 .and()
+                .csrf(
+                        csrf -> csrf.disable()
+                )
                 .formLogin()
                 .loginPage("/user/login").defaultSuccessUrl("/question/list")
                 .and()
@@ -33,7 +64,16 @@ public class SecurityConfig {
                 .invalidateHttpSession(true)
                 .and()
                 .exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler());
+                .accessDeniedHandler(accessDeniedHandler())
+                .and()
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/user/login")
+                        .userInfoEndpoint(
+                                userInfoEndpoint -> userInfoEndpoint
+                                        .userService(customOAuth2UserService)
+                        )
+			);
+
         return http.build();
     }
 
@@ -51,4 +91,23 @@ public class SecurityConfig {
     AccessDeniedHandler accessDeniedHandler() {
         return new CustomAccessDeniedHandler();
     }
+
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+//    }
+//    private ClientRegistration googleClientRegistration() {
+//        return ClientRegistration.withRegistrationId("kakao")
+//                .clientId("a0072d1885fe5cbccfeb539059439e9f")
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .redirectUri("http://localhost:8088/login/auth2/code/kakao")
+//                .scope("profile_nickname", "account_email")
+//                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+//                .tokenUri("https://kauth.kakao.com/oauth/token")
+//                .userInfoUri("https://kapi.kakao.com/v2/user/me")
+//                .userNameAttributeName(IdTokenClaimNames.SUB)
+//                .clientName("kakao")
+//                .build();
+//    }
 }
